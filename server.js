f2f78a19de629a8c43bfba8a0118f1095b78c683
Ivan Orellana Jr <iorellana21@gmail.com>
2021-03-09 18:07:04 -0500

    add public folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
