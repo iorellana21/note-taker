eed9f92e5db78a4c0cf3b255c5c6038cd36c80ce
Ivan Orellana Jr <iorellana21@gmail.com>
2021-03-08 23:32:34 -0500

    commenting out POST and DELETE

--- Develop/server.js
+++ Develop/server.js
@@ -25,15 +25,15 @@ app.get("/api/notes", function (req, res) {
     });
 });
 
-app.post("/api/notes", function (req, res) {
-    // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
-});
+// app.post("/api/notes", function (req, res) {
+//     // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
+// });
 
-app.delete("/api/notes/:id", function (req, res) {
-    // Should receive a query parameter containing the id of a note to delete
-    // need to find a way to give each note a unique `id` when it's saved
-    // need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file
-});
+// app.delete("/api/notes/:id", function (req, res) {
+//     // Should receive a query parameter containing the id of a note to delete
+//     // need to find a way to give each note a unique `id` when it's saved
+//     // need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file
+// });
 
 
 app.listen(PORT, function () {
