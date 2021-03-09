8d42fc63a5b7d0c9d5b91f24708254f1b5b6b21d
Ivan Orellana Jr <iorellana21@gmail.com>
2021-03-08 23:12:28 -0500

    updating server.js - including heroku in PORT

--- Develop/server.js
+++ Develop/server.js
@@ -4,8 +4,7 @@ const path = require('path');
 
 const app = express();
 
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
     console.log("main page");
